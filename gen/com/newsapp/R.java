7929052283a31a4b3222c8cdd499ae5bf0139342
ericlai <laisiutak@gmail.com>
2013-06-24 15:45:36 -0700

    add new icons for main page

--- gen/com/newsapp/R.java
+++ gen/com/newsapp/R.java
@@ -21,7 +21,25 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int abc01=0x7f020000;
+        public static final int bbc02=0x7f020001;
+        public static final int canoe03=0x7f020002;
+        public static final int cbc04=0x7f020003;
+        public static final int cnn05=0x7f020004;
+        public static final int cp24_06=0x7f020005;
+        public static final int ctv07=0x7f020006;
+        public static final int dailymail08=0x7f020007;
+        public static final int fox09=0x7f020008;
+        public static final int globeandmail10=0x7f020009;
+        public static final int google11=0x7f02000a;
+        public static final int ic_launcher=0x7f02000b;
+        public static final int nationalpost12=0x7f02000c;
+        public static final int nbc13=0x7f02000d;
+        public static final int nyt14=0x7f02000e;
+        public static final int reddit15=0x7f02000f;
+        public static final int theh16=0x7f020010;
+        public static final int thestar17=0x7f020011;
+        public static final int ussa18=0x7f020012;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
